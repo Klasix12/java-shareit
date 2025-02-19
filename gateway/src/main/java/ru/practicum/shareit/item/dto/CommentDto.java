ac169ecf5e7ff36aaf2da9a1a60eac8242684964
Artem Oleshev <sunlightpattern@yandex.ru>
2025-02-19 14:47:06 +0300

    refactor: add validation to CommentDto

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,7 +12,10 @@ import java.time.LocalDateTime;
 public class CommentDto {
     private Long id;
     private Long itemId;
+    @NotBlank
     private String authorName;
+    @NotBlank
+    @Size(max=2048)
     private String text;
     private LocalDateTime created;
 }
